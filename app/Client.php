73d70984cc4ed172baceb0350ecdec2c2de34759
Daniele Severiano <danisck@gmail.com>
2022-10-06 02:10:48 -0300

    Configurações iniciais do projeto

---
+++ app/Client.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    //
+}
+
+//Comando que cria a model e migration ao mesmo tempo: php artisan make:model Client -m
